6df8931b65eb7865d78e2c83aa382208f4b21fb6
maxwelladapoe <maxwelladapoe@wearepoe.com>
2021-03-31 23:05:28 +0000

    initial commit and ui designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <div className="calculator-wrap">
+                    <div className='screen'>
+                        <div className='input-stream'></div>
+                        <div className='compute'></div>
+                    </div>
+                    <div className='numbers-wrap'>
+                        <div className="number ">CE</div>
+                        <div className="number ">C</div>
+                        <div className="number "> B </div>
+                        <div className="number shadow yellow"> ÷ </div>
+                        <div className="number shadow">9</div>
+                        <div className="number shadow">8</div>
+                        <div className="number shadow">7</div>
+                        <div className="number shadow yellow">x</div>
+                        <div className="number shadow">6</div>
+                        <div className="number shadow">5</div>
+                        <div className="number shadow">4</div>
+                        <div className="number shadow yellow">-</div>
+                        <div className="number shadow">3</div>
+                        <div className="number shadow">2</div>
+                        <div className="number shadow">1</div>
+                        <div className="number shadow yellow">+</div>
+                        <div className="number shadow">0</div>
+                        <div className="number shadow">.</div>
+                        <div className="number shadow">+-</div>
+                        <div className="number shadow blue">=</div>
+                    </div>
+                </div>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
